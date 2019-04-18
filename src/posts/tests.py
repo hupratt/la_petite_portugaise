33e68735b9701e042cdb873651cf4c4f180959a7
hupratt <hp@hp.com>
2019-04-18 07:17:03 +0200

    fixed logo bug

--- src/posts/tests.py
+++ src/posts/tests.py
@@ -4,19 +4,19 @@ import requests
 
 
 def grab_sitemap(url):
-    r = requests.get(url)
-    sitemapTags = BeautifulSoup(r.text).find_all("url")
+    r = requests.get(url, verify=False)
+    sitemapTags = BeautifulSoup(r.text, features="html.parser").find_all("url")
     print("The number of sitemaps are {0}".format(len(sitemapTags)))
     liste, liste2= list(), list()
     for sitemap in sitemapTags:
         liste.append(sitemap.find_all('loc')[0])
     for i in liste:
-        liste2.append(requests.post(i.text).status_code)
+        liste2.append(requests.post(i.text, verify=False).status_code)
     return liste2
 
 def func():
     url = "https://www.lapetiteportugaise.eu/sitemap.xml"
-    url = "http://127.0.0.1:8000/sitemap.xml"
+    # url = "http://127.0.0.1:8000/sitemap.xml"
     liste = grab_sitemap(url)
     return liste
 

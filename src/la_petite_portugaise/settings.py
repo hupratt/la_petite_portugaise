14d0eb063cc0c2612f11926c07cf2d0478fd2b00
hupratt <hp@hp.com>
2019-04-28 12:33:30 +0200

    db details not working, fixed now i hope

--- src/la_petite_portugaise/settings.py
+++ src/la_petite_portugaise/settings.py
@@ -12,13 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 from django.utils.translation import ugettext_lazy as _
 import os
 
-enginedb = ''
-dbname = ''
-dbuser = ''
-dbpassword = ''
-hostip = ''
-pnumber = ''
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

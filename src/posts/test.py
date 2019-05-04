7740f22dba5c262d3813b73c50f8955a5a08682f
hupratt <hp@hp.com>
2019-05-04 19:39:12 +0200

    pre-commit

--- src/posts/test.py
+++ src/posts/test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpRequest
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
@@ -71,13 +72,11 @@ class ConsultSitemapTest(SimpleTestCase):
             Return: ['200','200','200']
             """
             liste = list()
-            # headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
             for i in input:
                 liste.append(self.client.get(i).status_code)
             return liste
 
         def grab_urls_from_sitemap(url):
-            # self.client.get('/en/')
             r = self.client.get(url)
             sitemapTags = BeautifulSoup(
                 r.content, features="html.parser").find_all("url")
@@ -87,12 +86,10 @@ class ConsultSitemapTest(SimpleTestCase):
                 for i in (sitemap.find_all('loc')):
                     liste.append(i.get_text())
             return liste
-        # url = "https://www.lapetiteportugaise.eu/sitemap.xml"
         url = "http://127.0.0.1:8000/sitemap.xml"
         url_list = grab_urls_from_sitemap(url)
         url_list = parse_url(url_list)
         status_list = verify_return_code(url_list)
-        # self.assertEqual(status_list,['200','200','200'])
         for i in status_list:
             assert i == 200
 

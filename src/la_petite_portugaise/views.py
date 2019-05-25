a4fb5ac197d5e36dda1378ffcc31f5e083de3217
hupratt <hp@hp.com>
2019-05-25 11:54:41 +0200

    testing sentry

--- src/la_petite_portugaise/views.py
+++ src/la_petite_portugaise/views.py
@@ -38,7 +38,7 @@ def contact(request):
     sent = False
     if request.method == 'POST':
         # Form was submitted
-        form = EmailPostForm(request.POST)
+        # form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data

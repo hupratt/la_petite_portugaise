2983d2aa063c55f434bf3859566f243d43e3c5ed
hupratt <hp@hp.com>
2019-05-25 11:53:09 +0200

    testing sentry

--- src/la_petite_portugaise/views.py
+++ src/la_petite_portugaise/views.py
@@ -26,7 +26,7 @@ class index(ListView):
         from django.utils.translation import get_language
         context = super().get_context_data(**kwargs)
         language = get_language()
-        liste_events_en = Post.objects.all().filter(tag='event').order_by('timestamp').filter(timestamp__gte = datetime.datetime.now(tz=(settings.TIME_ZONE)))  # pylint: disable=no-member
+        liste_events_en = Post.objects.all().filter(tag='event').order_by('timestamp').filter(timestamp__gte = datetime.datetime.now(tz=pytz.timezone(settings.TIME_ZONE)))  # pylint: disable=no-member
         if language == 'en':
             context['events'] = liste_events_en
         else:

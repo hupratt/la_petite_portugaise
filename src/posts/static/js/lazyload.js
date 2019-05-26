7654d306f6c07103eaf6bc6b35f1fdc7d3a32ca7
hupratt <hp@hp.com>
2019-05-26 15:20:07 +0200

    adding new pictures to the index page

--- src/posts/static/js/lazyload.js
+++ src/posts/static/js/lazyload.js
@@ -1,6 +1,7 @@
 // delete loading gif and lazy load pictures so that the website loads faster
+
 toad = () => {
-    const pic_lazyload = ['DSC_4853', 'DSC_4875', 'DSC_5034', 'DSC_4956', 'DSC_4967', 'DSC_5015']
+    const pic_lazyload = ['DSC_5074_','DSC_5070', 'DSC_5066','DSC_4853', 'DSC_4875', 'DSC_5034', 'DSC_4956', 'DSC_4967', 'DSC_5015']
     pic_lazyload.forEach(function (ele, index) {
         var src = "/static/images/" + ele + ".jpg"
         var item = ".item" + index + "-slick1"

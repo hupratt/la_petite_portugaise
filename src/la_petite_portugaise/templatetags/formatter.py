535b2326ff0137234337d6f5e660992db4811144
hupratt <hp@hp.com>
2019-05-30 18:03:11 +0200

    alternative to datetime formatting through template tag

--- src/la_petite_portugaise/templatetags/formatter.py
+++ src/la_petite_portugaise/templatetags/formatter.py
@@ -1,8 +1,16 @@
 from django import template
 # from django.template import Template
+from django.utils import formats
 
 register = template.Library()
 
 @register.filter(expects_localtime=True, name="formatter")
-def format_date(date):
-    return date.strftime("%B, %Y")
+def format_date(datetime_object):
+    # localize time to users
+    # import pytz
+    # from django.utils.timezone import localtime
+    # from django.conf import settings
+    # print(localtime(datetime_object, timezone=pytz.timezone(settings.TIME_ZONE)).strftime("%B, %Y"))
+    # date = formats.date_format(datetime_object, format="F, Y", use_l10n=True)
+    return datetime_object
+

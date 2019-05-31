b1e888b811c0eceb17ef3e54826d85cbff810ae0
hupratt <hp@hp.com>
2019-05-31 21:31:48 +0200

    should not trigger new release

--- src/la_petite_portugaise/wsgi.py
+++ src/la_petite_portugaise/wsgi.py
@@ -13,7 +13,6 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "la_petite_portugaise.settings")
 
 application = get_wsgi_application()

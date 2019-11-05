96455d9ed45dc4abfce595e71cb016cf05980b40
hupratt <hp@hp.com>
2019-11-05 23:12:46 +0100

    new post new settings

--- src/posts/static/vendor/countdowntime/david_bastos.js
+++ src/posts/static/vendor/countdowntime/david_bastos.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function($) {
   "use strict";
 
   function getTimeRemaining(endtime) {
@@ -8,27 +8,27 @@
     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     var days = Math.floor(t / (1000 * 60 * 60 * 24));
     return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
     };
   }
 
   function initializeClock(id, endtime) {
-    var daysSpan = $('.event-3 > .days');
-    var hoursSpan = $('.event-3 > .hours');
-    var minutesSpan = $('.event-3 > .minutes');
-    var secondsSpan = $('.event-3 > .seconds');
+    var daysSpan = $(".event-100 > .days");
+    var hoursSpan = $(".event-100 > .hours");
+    var minutesSpan = $(".event-100 > .minutes");
+    var secondsSpan = $(".event-100 > .seconds");
 
     function updateClock() {
       var t = getTimeRemaining(endtime);
 
       daysSpan.html(t.days);
-      hoursSpan.html(('0' + t.hours).slice(-2));
-      minutesSpan.html(('0' + t.minutes).slice(-2));
-      secondsSpan.html(('0' + t.seconds).slice(-2))
+      hoursSpan.html(("0" + t.hours).slice(-2));
+      minutesSpan.html(("0" + t.minutes).slice(-2));
+      secondsSpan.html(("0" + t.seconds).slice(-2));
 
       if (t.total <= 0) {
         clearInterval(timeinterval);
@@ -39,7 +39,6 @@
     var timeinterval = setInterval(updateClock, 1000);
   }
 
-  var deadline = new Date(Date.parse(new Date('2019-10-17T18:00:00')));
-  initializeClock('clockdiv', deadline);
-
+  var deadline = new Date(Date.parse(new Date("2019-11-10T15:00:00")));
+  initializeClock("clockdiv", deadline);
 })(jQuery);

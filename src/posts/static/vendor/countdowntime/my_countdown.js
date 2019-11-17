7e2be464583406563521307bae73996610ddab44
hupratt <hp@hp.com>
2019-11-17 11:08:55 +0100

    clean up names and clean import

--- src/posts/static/vendor/countdowntime/my_countdown.js
+++ src/posts/static/vendor/countdowntime/my_countdown.js
@@ -22,48 +22,45 @@
 
   function initializeClock() {
     event_ids.forEach(function(event_id) {
-      var arr = [];
+      var event_datetime_arr = [];
       $("input[type=hidden]").each(function() {
         if (
           $(this).attr("name") == "timestamp-minute" &&
           $(this).attr("id") == event_id
         ) {
-          arr[0] = $(this).val();
+          event_datetime_arr[0] = $(this).val();
         }
         if (
           $(this).attr("name") == "timestamp-hour" &&
           $(this).attr("id") == event_id
         ) {
-          arr[1] = $(this).val();
+          event_datetime_arr[1] = $(this).val();
         }
         if (
           $(this).attr("name") == "timestamp-day" &&
           $(this).attr("id") == event_id
         ) {
-          arr[2] = $(this).val();
+          event_datetime_arr[2] = $(this).val();
         }
         if (
           $(this).attr("name") == "timestamp-month" &&
           $(this).attr("id") == event_id
         ) {
-          arr[3] = $(this).val();
+          event_datetime_arr[3] = $(this).val();
         }
         if (
           $(this).attr("name") == "timestamp-year" &&
           $(this).attr("id") == event_id
         ) {
-          arr[4] = $(this).val();
+          event_datetime_arr[4] = $(this).val();
         }
-        // var event_id = $(this).attr("id");
-        // console.log(arr[0], arr[1], arr[2], event_id);
         if (
-          arr[0] !== undefined &&
-          arr[1] !== undefined &&
-          arr[2] !== undefined &&
-          arr[3] !== undefined &&
-          arr[4] !== undefined
+          event_datetime_arr[0] !== undefined &&
+          event_datetime_arr[1] !== undefined &&
+          event_datetime_arr[2] !== undefined &&
+          event_datetime_arr[3] !== undefined &&
+          event_datetime_arr[4] !== undefined
         ) {
-          // var arr = [undefined, undefined, undefined];
           var name = ".event-" + event_id;
           var daysSpan = $(name + "> .days");
           var hoursSpan = $(name + "> .hours");
@@ -73,15 +70,15 @@
           function updateClock() {
             var t = getTimeRemaining(
               new Date(
-                arr[4] +
+                event_datetime_arr[4] +
                   "-" +
-                  arr[3] +
+                  event_datetime_arr[3] +
                   "-" +
-                  arr[2] +
+                  event_datetime_arr[2] +
                   "T" +
-                  arr[1] +
+                  event_datetime_arr[1] +
                   ":" +
-                  arr[0] +
+                  event_datetime_arr[0] +
                   ":" +
                   "00"
               )

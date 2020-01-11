3261809b5bd8936dc0c0441f8d965813bcb135ce
hupratt <hprattdo@gmail.com>
2020-01-11 12:59:06 +0100

    fix bug events page not rendering

--- src/la_petite_portugaise/urls.py
+++ src/la_petite_portugaise/urls.py
@@ -28,9 +28,9 @@ urlpatterns += i18n_patterns(
     url(_('posts/'), include('posts.urls')),
     url(_('about-us/'),TemplateView.as_view(template_name="about-us.html"), name='about-us'),
     url(_('contact/'), views.contact, name='contact'),
-    url('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
-    url('', views.index.as_view(), name='index'),
-    url(_('events/'), list_events.as_view(), name='events'),
+    url(r'^sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    url(r'^$', views.index.as_view(), name='index'),
+    url(_('^events/$'), list_events.as_view(), name='events'),
 
     prefix_default_language=True)
 
